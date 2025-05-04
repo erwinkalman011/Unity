022b26d1aea25731f72728ff1726ae2da76cd176
Kalman Erwin <erwinkalman1@gmail.com>
2025-05-04 15:17:57 +0300

    Lab 3 EX 8

---
+++ Lab 3 EX 8/Assets/Scripts/MiscareSfera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MiscareSfera : MonoBehaviour
+{
+    public float fortaDeImpuls = 100;
+    public Transform cameraTransform;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float miscareOrizontala = Input.GetAxis("Horizontal");
+        float miscareVerticala = Input.GetAxis("Vertical");
+
+        // Obținem direcțiile camerei pe planul XZ
+        Vector3 camFata = cameraTransform.forward;
+        camFata.y = 0;
+        camFata.Normalize();
+
+        Vector3 camDreapta = cameraTransform.right;
+        camDreapta.y = 0;
+        camDreapta.Normalize();
+
+        // Direcția finală combinată
+        Vector3 directie = camFata * miscareVerticala + camDreapta * miscareOrizontala;
+
+        rb.AddForce(directie * fortaDeImpuls);
+    }
+}
